4c62ca0a4fcb1716b5fa21b30cca965d7b8e8758
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-06 05:10:35 +0100

    refactored code in script

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,19 +2,24 @@
 """Contains a function that determines if all lockboxes can be opened"""
 
 
-def canUnlockAll(boxes: list):
+def canUnlockAll(boxes: list) -> bool:
     """
     Determines if all lockboxes can be opened
     :param boxes: list of lists of numbers
     :return: if all lockboxes can be opened, true. Else, false
     """
-    openable = True
+    available_keys: list = []
 
     for index, box in enumerate(boxes):
-        if index > 0:
-            if index in box or box == []:
-                continue
-            else:
-                openable = False
-                break
-    return openable
+        for key in box:
+            if key not in available_keys:
+                available_keys.append(key)
+
+        # print('{} -> {}'.format(index, available_keys))
+
+        if index > 1 and index not in available_keys:
+            return False
+        else:
+            continue
+
+    return True
