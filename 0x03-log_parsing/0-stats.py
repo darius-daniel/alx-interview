42adc3b173101ac09efe3b20bb40dda6cac99931
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-22 23:03:32 +0100

    Overhauled the code in script 0-stat.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,57 @@
 #!/usr/bin/python3
-""" 0. Log Parsing"""
+""" 0. Log Parsing. """
 from sys import stdin
 from typing import Dict, Sequence
 
 
 possible_stat_codes: Sequence[int] = (200, 301, 400, 401, 403, 404, 405, 500)
 stat_codes_count: Dict[int, int] = {k: 0 for k in possible_stat_codes}
+total: Dict[str, int] = {'size': 0}
 
-total_size: int = 0
 
-
-def show_log(index: int, line: str, total_size: int) -> None:
+def print_stats(total_size: int) -> None:
     """
-    Generates the log file output
-    :param index: line number
-    :param line: context of line from stdin
-    :param total_size: total size in bytes
+    Prints total size and number of occurrences for each status code
+    :param total_size:
     :return: Nothing
     """
-    size = int(line.split(' ')[-1])
-    stat_code = int(line.split(' ')[-2])
-    total_size += size
+    print(f'File Size: {total_size}')
+    for stat_code in possible_stat_codes:
+        if stat_codes_count[stat_code] > 0:
+            print(f'{stat_code}: {stat_codes_count[stat_code]}')
 
-    if stat_code in possible_stat_codes and isinstance(stat_code, int):
+
+def show_log(line_num: int, current_size: int, stat_code: int) -> None:
+    """
+    Generates the log file output
+    :param line_num: line number
+    :param current_size:
+    :param stat_code:
+    :return: Nothing
+    """
+    total['size'] += current_size
+
+    if stat_code in possible_stat_codes:
         stat_codes_count[stat_code] += 1
 
-    if (index + 1) % 10 == 0 or line == '^C':
-        print(f'File Size: {total_size}')
-        for code in possible_stat_codes:
-            if stat_codes_count[code] > 0:
-                print(f'{code}: {stat_codes_count[code]}')
+    if line_num % 10 == 0:
+        print_stats(total['size'])
 
 
-try:
-    for index, line in enumerate(stdin):
-        show_log(index, line, total_size)
-except KeyboardInterrupt:
-    pass
+running: bool = True
+n_line: int = 1
+
+while running:
+    try:
+        line = stdin.readline()
+        size = int(line.split(' ')[-1])
+        code = int(line.split(' ')[-2])
+    except ValueError:
+        continue
+    except (KeyboardInterrupt, EOFError):
+        print_stats(total['size'])
+        running = False
+    else:
+        show_log(n_line, size, code)
+
+    n_line += 1
